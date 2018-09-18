befcb2cdfe4f3cb94d31eb53054bbd997e0ab930
Silas <silas.cutler@blacklistthisdomain.com>
2018-09-18 10:34:08 -0400

    Cleanup

---
+++ lnkparse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# By Silas Cutler
+#    silas.cutler@blacklistthisdomain.com
+
+import sys
+import lnkfile
+
+
+
+def main():
+	f = open(sys.argv[1], 'rb')
+        tmp = lnkfile.lnk_file(f , debug=True)
+        tmp.print_lnk_file()
+
+
+
+
+
+if __name__ == "__main__":
+	main()
+
+
